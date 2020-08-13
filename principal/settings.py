c6a1c7bfa981dc3dacabec07011d510b3bef83b7
anasmansouri <htmlphpcss3@gmail.com>
2020-08-13 18:21:20 +0100

    periodic feature and reading and writing from the sensor . not tested git add * !

--- principal/settings.py
+++ principal/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -27,7 +26,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+# redis server to do periodic tasks
+# Celery settings
+BROKER_URL = 'amqp://celeryuser@localhost:5672/celeryhost'
 
+# for security reasons, mention the list of accepted content-types (in this case json)
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_TIMEZONE = 'Europe/Berlin'
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +50,8 @@ INSTALLED_APPS = [
     'principal',
     'slaves_app',
     'rest_framework',
+    'django_celery_beat',
+    'django_celery_results'
 
 ]
 
@@ -73,7 +85,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'principal.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -83,7 +94,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -103,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -116,7 +125,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
---
+++ slaves_app/tasks.py
@@ -0,0 +1,20 @@
+from celery.schedules import crontab
+from celery.task import periodic_task
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+
+@shared_task
+def test_celery_worker():
+    f = open("./demofile2.txt", "a")
+    f.write("Now the file has more content!")
+    f.close()
+
+# A periodic task that will run every minute (the symbol "*" means every)
+# @periodic_task(run_every=(crontab(hour="*", minute="*", day_of_week="*")))
+# def task_example():
+#    f = open("./demofile2.txt", "a")
+#    f.write("Now the file has more content!")
+#    f.close()
