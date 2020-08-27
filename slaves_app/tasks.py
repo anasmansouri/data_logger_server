d90907e5d573e5087e3be59550f5eda1d160ea7c
anasmansouri <htmlphpcss3@gmail.com>
2020-08-27 17:31:00 +0100

    making the reading of values periodic

--- slaves_app/tasks.py
+++ slaves_app/tasks.py
@@ -3,11 +3,16 @@ from celery.task import periodic_task
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
+from slaves_app.functions import read_sensors_values
+from slaves_app.models import Slave
+
 logger = get_task_logger(__name__)
 
 
 @shared_task
 def test_celery_worker():
-    f = open("./demofile2.txt", "a")
-    f.write("Now the file has more content!")
+    enabled_slaves = Slave.get_enabled_slaves()
+    read_sensors_values(enabled_slaves)
+    f = open("./log_file.txt", "a")
+    f.write("value have been read successfully")
     f.close()
